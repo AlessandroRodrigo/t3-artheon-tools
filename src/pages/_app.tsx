6d9f53eabd50f836197cc96ab23d4a183a07d002
Alessandro Fresneda <alessandro.fresneda@codeandtheory.com>
2024-02-18 16:19:17 -0300

    Add font styling to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,26 @@
 import { type AppType } from "next/app";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "~/lib/utils";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Component
+      {...pageProps}
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased",
+        fontSans.variable,
+      )}
+    />
+  );
 };
 
 export default api.withTRPC(MyApp);
