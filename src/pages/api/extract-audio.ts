431d9d3132622cdda706be33d65e8bb7f77e2156
Alessandro Fresneda <alessandro.fresneda@codeandtheory.com>
2024-02-23 21:41:52 -0300

    Refactor audio extraction API

--- src/pages/api/extract-audio.ts
+++ src/pages/api/extract-audio.ts
@@ -1,13 +1,7 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
-import type formidable from "formidable";
 import { Formidable } from "formidable";
-import { PassThrough, Writable } from "stream";
 import { createReadStream } from "fs";
-import {
-  batchExtractAudio,
-  extractAudioStream,
-} from "~/server/services/extract-audio";
-import MultiStream from "multistream";
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { extractAudioStream } from "~/server/services/extract-audio";
 
 export const config = {
   api: {
@@ -20,8 +14,6 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   if (req.method === "POST") {
-    console.log("here...");
-
     const form = new Formidable({
       multiples: false,
       allowEmptyFiles: false,
@@ -42,37 +34,6 @@ export default async function handler(
     });
 
     await form.parse(req);
-
-    // form.parse(req, async (err, fields, files) => {
-    //   if (err) {
-    //     return res.status(500).json({ message: "Error parsing form" });
-    //   }
-    //   if (!files.file) {
-    //     return res.status(400).json({ message: "No file found" });
-    //   }
-    //   res.writeHead(200, { "Content-Type": "audio/mp3" });
-    //   const filesPath = files.file.map((file) => file.filepath);
-    //   const readStreams = filesPath.map((filePath) =>
-    //     createReadStream(filePath),
-    //   );
-    //   // void batchExtractAudio(readStreams);
-    //   if (!readStreams[0]) {
-    //     return res.status(400).json({ message: "No file found" });
-    //   }
-    //   const output = await batchExtractAudio(readStreams);
-    //   if (!output) {
-    //     return res.status(500).json({ message: "Error processing files" });
-    //   }
-    //   const multistream = new MultiStream(output);
-    //   // console.log(multistream);
-    //   multistream.pipe(
-    //     new Writable({
-    //       write: (chunk) => {
-    //         console.log(chunk);
-    //       },
-    //     }),
-    //   );
-    // });
   } else {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
