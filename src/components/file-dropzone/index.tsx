01f4e3a89141333fb3373125bd6c6a6991346a65
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2024-03-03 17:05:34 -0300

    Update file dropzone UI

--- src/components/file-dropzone/index.tsx
+++ src/components/file-dropzone/index.tsx
@@ -31,16 +31,37 @@ export function FileDropzone({ onChange, accept, files }: Props) {
     <div className="flex flex-col gap-4">
       <div
         {...getRootProps()}
-        className="flex cursor-pointer flex-col items-center justify-center gap-4 rounded-lg border-2 border-dashed border-gray-300 p-4 text-center text-gray-500 transition-colors hover:border-gray-500"
+        className="flex w-full items-center justify-center"
       >
-        <input {...getInputProps()} />
-        <File />
-
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>Drag and drop some video files here, or click to select files</p>
-        )}
+        <label
+          htmlFor="dropzone-file"
+          className="dark:hover:bg-bray-800 flex h-40 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        >
+          <div className="flex flex-col items-center justify-center pb-6 pt-5">
+            <svg
+              className="mb-3 h-10 w-10 text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+              ></path>
+            </svg>
+            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+              <span className="font-semibold">Click to upload</span> or drag and
+              drop
+            </p>
+            <p className="text-xs text-gray-500 dark:text-gray-400">
+              .mp4 files only
+            </p>
+          </div>
+          <input {...getInputProps()} />
+        </label>
       </div>
 
       <div>
