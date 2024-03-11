3f2801b0b55ffb92d5d4bf5d4757e312366ca11b
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2024-03-10 21:13:08 -0300

    Update file-dropzone component styles

--- src/components/file-dropzone/index.tsx
+++ src/components/file-dropzone/index.tsx
@@ -39,7 +39,7 @@ export function FileDropzone({ onChange, accept, files, validate }: Props) {
       >
         <label
           htmlFor="dropzone-file"
-          className="dark:hover:bg-bray-800 flex h-40 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+          className="dark:hover:bg-bray-800 flex h-40 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 transition-colors duration-200 hover:border-gray-500 dark:border-gray-700 dark:hover:border-gray-500"
         >
           <div className="flex flex-col items-center justify-center pb-6 pt-5">
             <Upload className="mb-4 h-6 w-6 text-gray-500 dark:text-gray-400" />
