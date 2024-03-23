d1ad2a8dae8ffe3116df5bd205aceb152193615f
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2024-03-23 13:17:05 -0300

    Add video format constants

---
+++ src/constants/index.ts
@@ -0,0 +1,14 @@
+const ACCEPTED_VIDEO_FORMATS = [
+  ".mp4",
+  ".webm",
+  ".ogg",
+  ".mov",
+  ".avi",
+  ".wmv",
+  ".flv",
+  ".mkv",
+];
+
+export const Constants = {
+  ACCEPTED_VIDEO_FORMATS,
+} as const;
