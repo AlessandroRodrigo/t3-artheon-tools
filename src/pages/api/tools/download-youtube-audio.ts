90947c117b3db666c9cab4cab06f0a690cbb85fb
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2024-03-24 20:16:15 -0300

    Add functionality to download YouTube audio and create a zip file

--- src/pages/api/tools/download-youtube-audio.ts
+++ src/pages/api/tools/download-youtube-audio.ts
@@ -1,6 +1,18 @@
+import AdmZip from "adm-zip";
+import { queue } from "async";
+import {
+  createWriteStream,
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  readFile,
+} from "fs";
+import { remove } from "fs-extra";
 import { type NextApiRequest, type NextApiResponse } from "next";
+import { escape } from "querystring";
 import ytdl from "ytdl-core";
 import { z } from "zod";
+import { logger } from "~/server/lib/logger";
 
 const RequestBodyParser = z.object({
   urls: z.string().url().array(),
@@ -23,10 +35,75 @@ export default async function handler(
       return res.status(400).json({ message: "No URLs provided" });
     }
 
-    ytdl(parserResult.data.urls[0], {
-      filter: "audioonly",
-      quality: "lowestaudio",
-    }).pipe(res);
+    if (!existsSync("./tmp")) {
+      mkdirSync("./tmp");
+    }
+
+    const processQueue = queue((url: string, callback) => {
+      ytdl
+        .getInfo(url)
+        .then((info) => {
+          const writeStream = createWriteStream(
+            `./tmp/${info.videoDetails.title}.mp3`,
+          );
+
+          const stream = ytdl(url, {
+            filter: "audioonly",
+            quality: "lowestaudio",
+          });
+
+          stream.on("end", () => {
+            callback();
+          });
+
+          stream.pipe(writeStream);
+        })
+        .catch((error) => {
+          logger.error(error);
+          callback(error as Error);
+        });
+    }, 3);
+
+    processQueue.error((error) => {
+      console.error(error);
+      res.end();
+    });
+
+    processQueue.drain(() => {
+      const zip = new AdmZip();
+      const files = readdirSync("./tmp");
+
+      files.forEach((file) => {
+        zip.addLocalFile(`./tmp/${file}`);
+      });
+
+      zip.writeZip("./tmp/result.zip");
+
+      readFile("./tmp/result.zip", (error, data) => {
+        if (error) {
+          logger.error(error);
+          res.end();
+        }
+
+        res.setHeader("Content-Type", "application/zip");
+        res.setHeader(
+          "Content-Disposition",
+          `attachment; filename=${escape("result.zip")}`,
+        );
+        res.send(data);
+        res.end();
+
+        remove("./tmp").catch((error) => {
+          logger.error(error);
+        });
+      });
+    });
+
+    parserResult.data.urls.forEach((url) => {
+      processQueue.push(url).catch((error) => {
+        console.error(error);
+      });
+    });
   } else {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
