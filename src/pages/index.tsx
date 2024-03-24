92b6268d4e95b8a99dd941fa8673bd618d52f7f3
AlessandroRodrigo <alessandro.fresneda84@gmail.com>
2024-03-24 15:23:24 -0300

    Update imports and add sign-in functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,13 @@
-import { ArrowUp, LockIcon, ArrowBigRight, Fingerprint } from "lucide-react";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import {
+  ArrowBigRight,
+  ArrowUp,
+  Fingerprint,
+  LockIcon,
+  LucideArrowRight,
+} from "lucide-react";
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
 
 export default function LandingPage() {
   return (
@@ -19,6 +28,7 @@ const navigation = [
 
 function HeroSection() {
   // const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const { isSignedIn } = useUser();
 
   return (
     <div className="bg-white">
@@ -38,14 +48,10 @@ function HeroSection() {
             </a>
           </div>
           <div className="flex lg:hidden">
-            <button
-              type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              // onClick={() => setMobileMenuOpen(true)}
-            >
+            <Button>
               <span className="sr-only">Open main menu</span>
               {/* <Bars3Icon className="h-6 w-6" aria-hidden="true" /> */}
-            </button>
+            </Button>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
@@ -58,13 +64,22 @@ function HeroSection() {
               </a>
             ))}
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a
-              href="#"
-              className="text-sm font-semibold leading-6 text-gray-900"
-            >
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
+          <div className="lg:flex lg:flex-1 lg:justify-end">
+            {isSignedIn ? (
+              <Link href="/tools" passHref>
+                <Button>
+                  Go to dashboard
+                  <LucideArrowRight
+                    className="ml-2 h-4 w-4"
+                    aria-hidden="true"
+                  />
+                </Button>
+              </Link>
+            ) : (
+              <SignInButton>
+                <Button>Sign in</Button>
+              </SignInButton>
+            )}
           </div>
         </nav>
       </header>
@@ -102,12 +117,7 @@ function HeroSection() {
               fugiat aliqua.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
+              <Button>Get started</Button>
               <a
                 href="#"
                 className="text-sm font-semibold leading-6 text-gray-900"
@@ -196,12 +206,7 @@ function PriceSection() {
                     USD
                   </span>
                 </p>
-                <a
-                  href="#"
-                  className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Get access
-                </a>
+                <Button className="mt-10">Get access</Button>
                 <p className="mt-6 text-xs leading-5 text-gray-600">
                   Invoices and receipts available for easy company reimbursement
                 </p>
