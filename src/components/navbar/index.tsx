cb54a891a2395232da019571edaeee68b2aa0784
Alessandro Rodrigo <97448512+AlessandroRodrigo@users.noreply.github.com>
2024-03-31 16:49:26 -0300

    Update index.tsx

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,8 +3,8 @@ import { type ComponentProps } from "react";
 
 export function Navbar() {
   return (
-    <nav className="flex items-center justify-between bg-primary p-4">
-      <h1 className="text-2xl font-bold text-white">Artheon</h1>
+    <nav className="bg- flex items-center justify-between border-b p-4">
+      <h1 className="text-2xl font-bold text-primary">Artheon</h1>
 
       <UserButton />
     </nav>
